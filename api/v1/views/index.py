c616b42263d612d3c4801bb1eb55796431c06d5a
lolade-olatunji <ololade0507@gmail.com>
2024-04-28 13:46:44 -0700

    task 2

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,6 @@
-s module contains endpoint(route) status
+#!/usr/bin/python3
+"""
+This module contains endpoint(route) status
 """
 from models import storage
 from flask import Flask
@@ -12,16 +14,3 @@ def status():
     Returns a JSON status
     """
     return jsonify({"status": "OK"})
-
-
-@app_views.route('/stats', strict_slashes=False)
-def count():
-    """
-    Retrieves the number of each objects by type
-    """
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
